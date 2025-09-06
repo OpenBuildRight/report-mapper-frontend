bfee75bf825f17a4ec0ef095e378d224f06573a2
DustinMoriarty <dustin.moriarty@protonmail.com>
2025-09-06 00:20:45 -0500

    Creating form

--- src/components/ObservationForm.tsx
+++ src/components/ObservationForm.tsx
@@ -1,35 +1,52 @@
-import {Card, Container, Grid, Paper, TextField} from "@mui/material";
+import {Card, Container, Grid, Paper, Stack, TextField} from "@mui/material";
 import { DateTimePicker } from '@mui/x-date-pickers/DateTimePicker';
 import {LocalizationProvider} from "@mui/x-date-pickers";
 import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs';
+import Button from "@mui/material/Button";
 
 const ObservationForm = () => {
     return (
-        <Container>
-            <Paper>
-                <TextField
-                    required={true}
-                    name={"Description"}
-                    label={"Description"}
-                />
-                <LocalizationProvider dateAdapter={AdapterDayjs}>
-                    <DateTimePicker
-                        name={"Observation Time"}
-                        label={"Observation Time"}
+        <Paper
+            sx = {{
+                alignItems: "center",
+                alignContent: "center",
+            }}
+            elevation={1}
+        >
+            <Stack direction={"column"} spacing={2}>
+                <Card
+                    elevation={10}
+
+
+                >
+                    <TextField
+                        required={true}
+                        name={"Description"}
+                        label={"Description"}
                     />
-                </LocalizationProvider>
-            </Paper>
+                    <LocalizationProvider dateAdapter={AdapterDayjs}>
+                        <DateTimePicker
+                            name={"Observation Time"}
+                            label={"Observation Time"}
+                        />
+                    </LocalizationProvider>
+                </Card>
+                <Card>
+                    <TextField type="number"
+                               name={"Latitude"}
+                               label={"Latitude"}
+                    />
+                    <TextField type="number"
+                               name={"Longitude"}
+                               label={"Longitude"}
+                    />
+                </Card>
+
+            </Stack>
             <Card>
-                        <TextField type="number"
-                                   name={"Latitude"}
-                                   label={"Latitude"}
-                        />
-                        <TextField type="number"
-                                   name={"Longitude"}
-                                   label={"Longitude"}
-                        />
+                <Button variant={"outlined"}>Submit</Button>
             </Card>
-        </Container>
+        </Paper>
     )
 }
 
