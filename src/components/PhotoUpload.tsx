1734726a7fc3944edac1a81e0a605bcc80fd390e
DustinMoriarty <dustin.moriarty@protonmail.com>
2025-09-07 20:42:28 -0500

    Add photo upload

--- src/components/PhotoUpload.tsx
+++ src/components/PhotoUpload.tsx
@@ -1,26 +1,39 @@
-import {Card, Input, Paper, TextField} from "@mui/material"
+import {Card, Paper, TextField} from "@mui/material"
+import React, {useState} from "react";
 
-const PhotoUpload = (
+const PhotoUpload : React.FC = (
 
 ) => {
+    const fileRef = React.useRef(null);
+    const [files, setFiles] = useState<File[]>([]);
+    function handleFileChange(event: React.ChangeEvent<HTMLInputElement>) {
+        if (event.target.files) {
+            setFiles(Array.from(event.target.files));
+        }
+    }
     return (
         <Paper>
             <Card elevation={10}>
                 <div>Upload Photos</div>
-                <TextField
+                <input
                     type="file"
-                    slotProps={
-                        {
-                            input : {
-                                inputProps : {
-                                    accept: "image/jpg, image/jpeg, image/png",
-                                    multiple: true
-                                }
-                            }
-                        }
-
-                    }
+                    ref={fileRef}
+                    multiple={true}
+                    accept="image/jpg, image/jpeg, image/png"
+                    onChange={(e) => {handleFileChange(e)}}
                 />
+                <div>
+                    {files.map((file, index) => (
+                        <img
+                            key={index}
+                            alt={file.name}
+                            src={URL.createObjectURL(file)}
+                            style={{
+                                maxWidth: "100px",
+                            }}
+                        />
+                    ))}
+                </div>
             </Card>
         </Paper>
     )
